c57faff733ec7073f54c4aedf46b78ded01d65d8
San Lin Htike <sanlinhtike@Sans-MacBook-Air.local>
2023-12-18 21:49:19 +0630

    add metadata

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,23 +2,23 @@ import Link from 'next/link'
 import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 
-export function PostCard(post: Post) {
-    return (
-        <div className="mb-8">
-            <h2 className="mb-1 text-xl">
-                <Link
-                    href={post.url}
-                    className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-                >
-                    {post.title}
-                </Link>
-            </h2>
-            <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-                {new Intl.DateTimeFormat("en-US").format(new Date(post.date))}
-            </time>
-        </div>
-    );
-}
+// export function PostCard(post: Post) {
+//     return (
+//         <div className="mb-8">
+//             <h2 className="mb-1 text-xl">
+//                 <Link
+//                     href={post.url}
+//                     className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
+//                 >
+//                     {post.title}
+//                 </Link>
+//             </h2>
+//             <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+//                 {new Intl.DateTimeFormat("en-US").format(new Date(post.date))}
+//             </time>
+//         </div>
+//     );
+// }
 
 export default function Home() {
     const posts = allPosts.sort(
@@ -30,9 +30,9 @@ export default function Home() {
             <h1 className="mb-8 text-center text-2xl font-black">
                 Next.js + Contentlayer Example
             </h1>
-            {posts.map((post, idx) => (
+            {/* {posts.map((post, idx) => (
                 <PostCard key={idx} {...post} />
-            ))}
+            ))} */}
         </div>
     );
 }
