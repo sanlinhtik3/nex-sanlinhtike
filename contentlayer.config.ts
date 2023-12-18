ce728956c8aee39ac17d5c8cfa95a78e4d455690
San Lin Htike <sanlinhtike@Sans-MacBook-Air.local>
2023-12-18 21:51:48 +0630

    add metadata

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -33,6 +33,6 @@ export default makeSource({
     documentTypes: [Post],
     mdx: {
         remarkPlugins: [remarkGfm],
-        rehypePlugins: [highlight],
+        rehypePlugins: [],
     },
 });
