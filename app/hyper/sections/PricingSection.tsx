ac7b9a110fdc845f973e59e985ee25f8e5b05a8a
San Lin Htike <sanlinhtike@Sans-MacBook-Air.local>
2023-12-20 16:33:50 +0630

    add book link

--- app/hyper/sections/PricingSection.tsx
+++ app/hyper/sections/PricingSection.tsx
@@ -14,6 +14,47 @@ export default function PricingSection() {
 
                 <div className="">
                     <div className="grid md:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0 gap-y-10">
+                        <div className=" col-span-2 flex flex-col p-6 mx-auto w-full text-gray-300  border border-gray-800 rounded-xl shadow dark:border-gray-600 xl:p-8  dark:text-white hover:border-sky-500">
+                            <h3 className="mb-4 text-2xl font-semibold">Buy Online Course Book</h3>
+                            <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                                Web Development နှင့်ပတ်သက်တဲ့ သင်ခန်းစာများ၊ Topic, Lession များကို အသေးစိတ်လေ့လာရမှာဖြစ််ပြီး Online Course Book တစ်ခုလုံးကိုမှာ 1.99 USDT ဖြစ်ပြီး မြန်မာငွေ 7,000 MMK ကျသင့်မှာဖြစ်ပါတယ်။ သင်ခန်းစာများသည် Web Development ကို တီးမိခေါက်မိရှိသူများအတွက် လုပ်ငန်းခွင်ဝင်နိုင်ဖို့အထိ ရည်ရွယ်ပြီး ခုမှလေ့လာမည့်သူများက HTML, CSS လို Basic သင်ခန်းစားကို အရင်လေ့လာရမှာဖြစ်ပါတယ်။ အဲ့ဒီသင်ခန်းစာများအတွက် ၁၀ ရက်လောက်ကြာနိုင်မှာဖြစ်ပြီး <Link href={"https://www.youtube.com/sanlinhtike"} className=" underline text-sky-500">ဒီမှာ</Link> လေ့လာနိုင်ပါတယ်။ စာအုပ်မှာ အသေးစိတ် Detail ကို အချိန်တိုတိုနဲ့ နားလည်လွယ်အောင်ရှင်းပြထားတာဖြစ်ပြီး ဝယ်ယူလိုပါက <Link href={"https://m.me/inficreax"} className=" underline text-sky-500">ဒီလင့်</Link> ကို Click ကာ ဝယ်ယူနိုင်ပါတယ်။
+                            </p>
+                            <div className="flex items-baseline my-8">
+                                <span className="mr-2 text-5xl font-extrabold text-sky-500">$1.99</span>
+                                <span className="text-gray-500 dark:text-gray-400">/hr</span>
+                            </div>
+                            <ul role="list" className="mb-8 space-y-4 text-left">
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>JavaScript</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>TailwindCSS</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>React</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>Next.js</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>Git & Github</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>Support life time</span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <svg className="flex-shrink-0 w-5 h-5 text-sky-500 dark:text-sky-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                                    <span>Free updates: <span className="font-semibold">lifetime</span></span>
+                                </li>
+                            </ul>
+                            <Link href="#" className="text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:ring-sky-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-sky-900">Get started</Link>
+                        </div>
                         <div className="flex flex-col p-6 mx-auto w-full text-gray-300  border border-gray-800 rounded-xl shadow dark:border-gray-600 xl:p-8  dark:text-white hover:border-sky-500">
                             <h3 className="mb-4 text-2xl font-semibold">Starter</h3>
                             <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p>
