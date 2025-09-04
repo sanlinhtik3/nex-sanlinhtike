c95b20eff1aa6c6e3efb50ab99706e32c4e16874
San Lin Htike <sanlin.htik3@gmail.com>
2025-09-04 20:53:20 +0700

    update telegram

--- app/hyper/sections/IntroSection.tsx
+++ app/hyper/sections/IntroSection.tsx
@@ -27,7 +27,7 @@ export const projects = [
     title: "Telegram",
     description:
       "This Telegram Channel will help you become a Crypto Expert in Binance, Real Time Crypto News, DeFi, NFT, GameFi, SocialFi, Metaverse, and more.",
-    link: "https://t.me/@zoecryptofree",
+    link: "t.me/zoecryptofree",
   },
   {
     title: "TikTok",
